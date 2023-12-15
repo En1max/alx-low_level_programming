43321699629a63808029969da48f585946cd7bff
En1max <moumne2255@gamil.com>
2023-12-14 18:44:28 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
  * entry main
  * Description: C program that prints with puts function
  * Return: 0 if succesful
- */
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
